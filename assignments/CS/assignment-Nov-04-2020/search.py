e908017ca9478c46a786971c24b734eaa6a34226
bewakes <bewakepandey@gmail.com>
2020-11-02 10:49:14 +0545

    Add parallel code, but is slower

--- assignments/CS/assignment-Nov-04-2020/search.py
+++ assignments/CS/assignment-Nov-04-2020/search.py
@@ -1,6 +1,8 @@
+import os
+import math
 import time
 import sys
-from multiprocessing import Process
+import multiprocessing as mp
 
 
 class log_time:
@@ -20,21 +22,42 @@ def read_data(fname='numbers.data'):
     return data
 
 
-def search_number(data, number):
-    appearances = 0
+def search_number(data, number, start=None, end=None, queue=None):
+    total_count = 0
     first_index = None
-    for i, x in enumerate(data):
-        if x == number and first_index is None:
+    start = start or 0
+    end = min(end or len(data), len(data))
+    for i in range(start, end):
+        if data[i] == number and first_index is None:
             first_index = i
-        elif x == number:
-            appearances += 1
-    return (first_index, appearances)
+        elif data[i] == number:
+            total_count += 1
+    if queue:
+        queue.put((first_index, total_count))
+    else:
+        return (first_index, total_count)
 
 
-def run_parallel(number):
+def run_parallel(number, num_processes=5):
     with log_time('read_data'):
         data = read_data()
-    print('parallel')
+
+    chunk_size = math.ceil(len(data) / num_processes)
+    queue = mp.Queue()
+
+    for i in range(num_processes):
+        p = mp.Process(target=search_number, args=(data, number, i*chunk_size, (i+1)*chunk_size, queue))
+        p.start()
+        p.join()
+
+    # Now collect data
+    first_index = None
+    total_count = 0
+    for i in range(num_processes):
+        ind, cnt = queue.get()
+        total_count += cnt
+        first_index = ind if first_index is None or ind < first_index else first_index
+    print((first_index, total_count))
 
 
 def run_sequential(number):

e78a640541cc7f649c289374d4e69aa14c59e633
bewakes <bewakepandey@gmail.com>
2020-11-05 12:25:43 +0545

    Add jupyter notebook and a graph

--- assignments/CS/assignment-Nov-04-2020/run.sh
+++ assignments/CS/assignment-Nov-04-2020/run.sh
@@ -2,11 +2,12 @@
 set -e
 
 N=${2:-1}
+d=${3:-100000000}
 
 for x in {1..8}; do
     s=0
     for y in $(seq 1 $N); do
-        t=`python search.py -n $1 -p $x | grep seconds | grep -o '[0-9]\+.[0-9]\+'`
+        t=`python search.py -n $1 -p $x -d $d | grep seconds | grep -o '[0-9]\+.[0-9]\+'`
         s=`awk -v s=$s -v t=$t 'BEGIN { print s+t*1000 }'`
     done
     avg=`awk -v s=$s -v N=$N 'BEGIN { print s / N }'`
